219a5342d89877928d3c01758e8b467dc128ddd5
Renan Sies <renansies@gmail.com>
2019-02-10 21:00:07 -0200

    removendo css e spec inutilizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EmployeeNewComponent } from './employee-new/employee-new.component';
-import { SalaryColorDirective } from './salary-color.directive';
-import { EmployeeNewModalComponent } from './employee-new-modal/employee-new-modal.component';
-import { AlertSuccessComponent } from './alert-success/alert-success.component';
-import { EmployeeEditModalComponent } from './employee-edit-modal/employee-edit-modal.component';
-import { EmployeeDeleteModalComponent } from './employee-delete-modal/employee-delete-modal.component';
-import { MyCurrencyPipe } from './my-currency.pipe';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeNewComponent } from './components/employee-new/employee-new.component';
+import { SalaryColorDirective } from './directives/salary-color.directive';
+import { EmployeeNewModalComponent } from './components/employee-new-modal/employee-new-modal.component';
+import { AlertSuccessComponent } from './components/alert-success/alert-success.component';
+import { EmployeeEditModalComponent } from './components/employee-edit-modal/employee-edit-modal.component';
+import { EmployeeDeleteModalComponent } from './components/employee-delete-modal/employee-delete-modal.component';
+import { MyCurrencyPipe } from './pipes/my-currency.pipe';
 
 @NgModule({
   declarations: [
--- src/app/salary-color.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SalaryColorDirective } from './salary-color.directive';
-
-describe('SalaryColorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SalaryColorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
